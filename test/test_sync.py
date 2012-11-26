2d86935f4b428450d459d1767b04871ba99017b7
Dionysis Zindros <dionyziz@gmail.com>
2012-11-26 11:01:38 -0500

    Syncing testcases.
    * Modularization of testing by using helper functions
    * Formatting changes to conform to PEP8
    * Modify the API to include WorkingCopy objects
    * Push and pull tests

--- test/test_sync.py
+++ test/test_sync.py
@@ -1,4 +1,5 @@
 from kamaki.clients.pithos import PithosClient as pithos, ClientError
+import filecmp
 import pithossync
 import unittest
 import os
@@ -12,21 +13,25 @@ class TestSync(unittest.TestCase):
             if container['name'] == name:
                 return True
         return False
+
     def object_exists(self, name):
         try:
             self.client.get_object_info(name)
         except ClientError:
             return False
         return True
+
     def is_folder(self, name):
         type = self.client.get_object_info(name)['content-type'] 
         return type == 'application/directory' or type == 'application/folder'
+
     def folder_empty(self, name):
         obj_list = self.client.list_objects()
         for obj in obj_list:
             if obj['name'][0:len(name + '/')] == name + '/':
                 return False
         return True
+
     def remote_recursive_delete(self, name):
         obj_list = self.client.list_objects()
         # delete all the folders' contents
@@ -35,8 +40,15 @@ class TestSync(unittest.TestCase):
                 self.client.object_delete(obj['name'])
         # delete the folder itself
         self.client.object_delete(name)
+
     def pprint(self, name, data):
         print json.dumps({name: data}, indent=4)
+
+    def assertTreesEqual(self, a, b ):
+        comparator = filecmp.dircmp(a, b)
+        self.assertEqual(len(comparator.left_only), 0)
+        self.assertEqual(len(comparator.right_only), 0)
+
     def setUp(self):
         # set up useful settings variables
         self.url = 'https://pithos.okeanos.grnet.gr/v1'
@@ -44,8 +56,11 @@ class TestSync(unittest.TestCase):
         self.account = 'dionyziz@gmail.com'
         self.container = 'pithos'
         # self.syncer = pithossync.Syncer(url, token, account, self.container)
+        # the local folder used by the syncing client
         self.local = 'localmirror'
+        # the local folder used by the tests 
         self.local_workspace = 'localworkspace'
+        # the name of the remote folder within the pithos container
         self.folder = 'sync-test'
         self.client = pithos(self.url, self.token, self.account, self.container)
 
@@ -68,13 +83,11 @@ class TestSync(unittest.TestCase):
             print('You can do this by running:')
             print('ASTAKOS_TOKEN="your_token_here" python runtests.py\n')
 
-    # def test_hello(self):
-    #     print('Hello world!')
     def test_empty_clone(self):
         """Check if cloning an empty folder works."""
 
         os.mkdir(self.local)
-        open(self.local + '/dummy', 'w').close()
+        self.local_write_file('dummy', '')
         self.assertRaises(
             pithossync.DirectoryNotEmptyError,
             self.syncer.clone,
@@ -85,12 +98,10 @@ class TestSync(unittest.TestCase):
         self.syncer.clone(self.local, self.folder)
         # make sure the directory is still empty after cloning an empty
         # server-side directory
-        for root, dirs, files in os.walk(self.local, topdown=False):
-            self.assertEqual(root, self.local)
-            self.assertEqual(len(files), 0)
-            self.assertEqual(len(dirs), 0)
+        self.assertTreesEqual(self.local, self.local_workspace)
         # make sure the server-side directory is not affected by the clone operation
         self.assertTrue(self.folder_empty(self.folder))
+
     def test_clone_one_text(self):
         """Check if cloning a folder containing a single text file works.
         
@@ -99,47 +110,185 @@ class TestSync(unittest.TestCase):
         """
 
         os.mkdir(self.local_workspace)
-        hello = 'Hello, world!\n'
-        f = open(self.local_workspace + '/one.txt', 'w')
-        f.write(hello)
-        f.close()
-
-        f = open(self.local_workspace + '/one.txt', 'r')
-        self.client.upload_object(self.folder + '/one.txt', f)
-        f.close()
+        self.workspace_write_file('one.txt', 'Hello, world!\n')
 
         os.mkdir(self.local)
         self.syncer.clone(self.local, self.folder)
 
-        for root, dirs, files in os.walk(self.local):
-            # no subdirectories should have been created
-            self.assertEqual(root, self.local)
-            self.assertEqual(len(dirs), 0)
-            # only one file should be created
-            self.assertEqual(len(files), 1)
-            self.assertEqual(files[0], 'one.txt')
+        self.assertTreesEqual(self.local, self.local_workspace)
 
-        self.assertTrue(os.path.exists(self.local + '/one.txt'))
-        f = open(self.local + '/one.txt', 'r')
-        contents = f.read()
-        self.assertEqual(contents, hello)
-    # def test_clone_OneBin(self):
-    # def test_clone_OneBig(self):
-    # def test_push(self):
-    #     pass
-    # def test_pull(self):
-    #     pass
+    def workspace_create_random(self, name, size):
+        try:
+            os.mkdir(self.local_workspace)
+        except OSError:
+            # directory already exists
+            pass
+
+        f = open(self.local_workspace + '/' + name, 'w')
+        for i in xrange(size / 8):
+            f.write(8 * '\0')
+        f.flush()
+        f.close()
+
+    def workspace_download(self, name):
+        f = open(self.local_workspace + '/' + name, 'wb+')
+        self.client.download_object(self.folder + '/' + name, f)
+        f.close()
+
+    def workspace_upload(self, name):
+        f = open(self.local_workspace + '/' + name, 'r')
+        self.client.upload_object(self.folder + '/' + name, f)
+        f.close()
+
+    def test_clone_one_bin(self):
+        """Check if cloning a folder with a single binary file works."""
+        self.workspace_create_random('test.bin', 8)
+        self.workspace_upload('test.bin')
+
+        os.mkdir(self.local)
+        self.syncer.clone(self.local, self.folder)
+        self.assertTreesEqual(self.local, self.local_workspace)
+
+    def test_clone_one_big(self):
+        """Check if cloning a folder with a 100MB binary file works."""
+
+        pass # too slow for now
+
+        self.workspace_create_random('test.bin', 100 * 1024 * 1024)
+        self.workspace_upload('test.bin')
+
+        os.mkdir(self.local)
+        self.syncer.clone(self.local, self.folder)
+        self.assertTreesEqual(self.local, self.local_workspace)
+
+    def write_file(self, name, contents):
+        f = open(name, 'w')
+        f.write(contents)
+        f.close()
+
+    def local_write_file(self, name, contents):
+        self.write_file(self.local + '/' + name, contents)
+
+    def workspace_write_file(self, name, contents):
+        self.write_file(self.local_workspace + '/' + name, contents)
+
+    def create_tree(self, file_list):
+        try:
+            os.mkdir(self.local_workspace)
+        except:
+            pass
+        for file in file_list:
+            path = file.split('/')
+            components = ''
+            for component in path[:-1]:
+                components += component
+                try:
+                    os.mkdir(self.local_workspace + '/' + components)
+                except OSError:
+                    # directory already exists
+                    pass
+                components += '/'
+            f = open(self.local_workspace + '/' + file, 'w')
+            f.write(file)
+            f.close()
+
+    def test_clone_tree(self):
+        """Create a tree of files and directories and check if it clones."""
+        self.create_tree(['red.file', 'green.file', 'blue.file',
+                          'foo/red.file', 'foo/green.file', 'foo/blue.file',
+                          'bar/quux/koko.txt', 'bar/quux/lala.txt', 'bar/quux/liruliru.txt'])
+        os.mkdir(self.local)
+        self.syncer.clone(self.local, self.folder)
+        self.assertTreesEqual(self.local, self.local_workspace)
+
+    def test_push_without_clone(self):
+        os.mkdir(self.local)
+        self.assertRaises(
+            pithossync.NoCloneError,
+            self.syncer.push,
+            self.local
+        )
+
+    def test_push_one_created(self):
+        os.mkdir(self.local)
+        os.mkdir(self.local_workspace)
+
+        # sync an empty directory from the server
+        working_copy = self.syncer.clone(self.local, self.folder)
+
+        # create a text file in our local mirror
+        self.local_write_file('one.txt', 'Hello, world')
+        # push our changes (file creation) to the server
+        working_copy.push()
+
+        # the file should now be uploaded to the server
+        # directly download the file using the pithos client
+        # to verify we have succeeded
+        self.workspace_download('one.txt')
+
+        # make sure the exact same file was downloaded
+        self.assertTreesEqual(self.local, self.local_workspace)
+
+    def test_push_one_modified(self):
+        os.mkdir(self.local_workspace)
+
+        os.mkdir(self.local)
+        working_copy = self.syncer.clone(self.local, self.folder)
+
+        # create a file in our local mirror
+        self.local_write_file('one.txt', 'Hello, world!\n')
+        # push it to the server
+        working_copy.push()
+
+        # modify the file
+        self.local_write_file('one.txt', 'Goodbye, world!\n')
+        # push it to the server
+        working_copy.push()
+        # download it independently
+        self.workspace_download('one.txt')
+
+        # make sure the modified file has been synchronized
+        self.assertTreesEqual(self.local, self.local_workspace)
+
+    def test_pull_one_created(self):
+        os.mkdir(self.local_workspace)
+        os.mkdir(self.local)
+        working_copy = self.syncer.clone(self.local, self.folder)
+
+        self.workspace_write_file('one.txt', 'Goodbye, world!\n')
+        self.workspace_upload('one.txt')
+
+        working_copy.pull()
+
+        self.assertTreesEqual(self.local, self.local_workspace)
+
+    def test_pull_one_modified(self):
+        os.mkdir(self.local_workspace)
+
+        self.workspace_write_file('one.txt', 'Hello, world!\n')
+
+        os.mkdir(self.local)
+        working_copy = self.syncer.clone(self.local, self.folder)
+
+        self.workspace_write_file('one.txt', 'Goodbye, world!\n')
+        self.workspace_upload('one.txt')
+
+        self.syncer.pull(self.local)
+
+        self.assertTreesEqual(self.local, self.local_workspace)
     # def test_sync(self):
     #     pass
+
     def local_recursive_delete(self, folder):
         """rm -rf folder"""
         if os.path.exists(folder):
             for root, dirs, files in os.walk(folder, topdown=False):
                 for name in files:
                     os.remove(os.path.join(root, name))
-                    for name in dirs:
-                        os.rmdir(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
             os.rmdir(folder)
+
     def tearDown(self):
         """Clean up all temporary directories on the server and on the client.
         
